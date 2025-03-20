71ed95c18b550810074667bae1e9d20c4755d094
Krypton321123 <coolrajsharma28@gmail.com>
2025-03-21 02:58:27 +0530

    GENAI FIXED

--- Backend/src/chats/index.ts
+++ Backend/src/chats/index.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from 'express'; 
 import { GoogleGenerativeAI } from '@google/generative-ai'
+import dotenv from 'dotenv'
 
 const chatRouter = express.Router(); 
+
+dotenv.config(); 
+
+
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || ""); 
 const model = genAI.getGenerativeModel(
@@ -10,12 +15,14 @@ const model = genAI.getGenerativeModel(
 
 
 chatRouter.route('/create').post(async (req: Request, res: Response) => {
+    const { userMessage }= req.body; 
+ 
 
     let AIResponse = ''
 
     try {
         const prompt = `Imagine you're a AI Therapist. A person comes into your office
-                and says "My wife divorced me"
+                and says "${userMessage}"
                 Give me a response in the following json format: 
                 {
                     WellnessScore: should be a number out of 100 judged by you on their well being
@@ -25,9 +32,14 @@ chatRouter.route('/create').post(async (req: Request, res: Response) => {
                     Please strictly follow the Format I provided. Don't give anything apart from that
                 `
 
-    model.generateContent(prompt).then((result) => {
-        AIResponse = result.response.text(); 
+    await model.generateContent(prompt).then(async (result) => {
+        const cleanJsonString = result.response.text().replace(/```json|```/g, '').trim();
+        
+        const response = await JSON.parse(cleanJsonString)
+        AIResponse = response.response
     }); 
+
+    console.log(AIResponse)
     return res.status(200).json({status: true, message: "Response genreated successfully", response: AIResponse})
         } catch (err) {
             console.log("generation error: ", err)
